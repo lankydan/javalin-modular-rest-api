ee7b1fa7858cbe2a2ec4b676e20ed08119e5cb19
LankyDan <danknewton@hotmail.com>
2021-11-07 15:12:58 +0000

    Use properties file for DB settings

--- application/src/main/kotlin/dev/lankydan/application/Application.kt
+++ application/src/main/kotlin/dev/lankydan/application/Application.kt
@@ -7,19 +7,18 @@ import io.javalin.apibuilder.ApiBuilder.path
 import org.jetbrains.exposed.sql.Database
 import org.kodein.di.DI
 import org.kodein.di.allInstances
+import java.util.Properties
+
+class Application
 
 fun main() {
 
-    Database.connect(
-        url = "jdbc:postgresql://localhost:5432/postgres",
-        driver = "org.postgresql.Driver",
-        user = "admin",
-        password = "admin"
-    )
+    connectToDatabase()
 
     val di = DI {
         import(People.module)
     }
+
     Javalin.create { config ->
         // Turned on as it's an example and makes it easier to see what's going on
         config.enableDevLogging()
@@ -29,4 +28,16 @@ fun main() {
             controllers.forEach { path(it.path, it.endpoints) }
         }
     }.start(8080)
+}
+
+private fun connectToDatabase() {
+    Properties().run {
+        load(Application::class.java.classLoader.getResourceAsStream("application.properties"))
+        Database.connect(
+            url = getProperty("database.url"),
+            driver = getProperty("database.driver"),
+            user = getProperty("database.user"),
+            password = getProperty("database.password")
+        )
+    }
 }
